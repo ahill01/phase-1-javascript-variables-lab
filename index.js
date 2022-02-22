834feee12e6e1b755dc23108644059c5cf420904
Allison <amh03160@gmail.com>
2022-02-22 10:44:12 -0600

    commit tree 8269fb5d5d04f21c4f232d90e467ec2876157882

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file!
+
+// companyName is Scuber, is defined as a const
+
+const companyName = "Scuber"; 
+
+//mostProfitableNeighborhood is Chelsea, defined using let
+
+let mostProfitableNeighborhood = "Chelsea";
+
+//companyCeo is Susan Smith, defined using let
+
+let companyCeo = "Susan Smith";
